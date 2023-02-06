db04dcce7b8dd9afa1a89c5d820b9ca8f27385d8
JuniorLicassali <licassali88@gmail.com>
2023-02-06 16:24:17 -0300

    exception handling

--- src/main/java/com/jrweb/servico/config/TesteConfig.java
+++ src/main/java/com/jrweb/servico/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.jrweb.servico.entities.Category;
 import com.jrweb.servico.entities.Order;
 import com.jrweb.servico.entities.OrderItem;
+import com.jrweb.servico.entities.Payment;
 import com.jrweb.servico.entities.Product;
 import com.jrweb.servico.entities.User;
 import com.jrweb.servico.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 }
---
+++ src/main/java/com/jrweb/servico/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.jrweb.servico.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.jrweb.servico.services.exceptions.DatabaseException;
+import com.jrweb.servico.services.exceptions.ResourceNotFoundExceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundExceptions.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundExceptions e, HttpServletRequest request) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
